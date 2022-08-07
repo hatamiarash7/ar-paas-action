d26bf622dc723be43843d95f63c881a6d06ad000
Arash Hatami <hatamiarash7@gmail.com>
2022-08-07 12:03:00 +0430

    move wget to bash script

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,5 @@
 FROM alpine:3.10
 
-RUN echo $CLI_VERSION 
-
-ARG CLI_VERSION
-ENV CLI_VERSION $CLI_VERSION
-
-RUN mkdir -p /service
-
-ADD https://github.com/arvancloud/cli/releases/download/v${CLI_VERSION}/arvan_${CLI_VERSION}_linux_amd64.tar.gz /service/
-
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+CLI_VERSION="$1"
+AUTH="$2"
+APP="$3"
+CONTAINER="$4"
+IMAGE="$5"
+
+echo "Hello"
+
+mkdir -p /service
+
+wget https://github.com/arvancloud/cli/releases/download/v${CLI_VERSION}/arvan_${CLI_VERSION}_linux_amd64.tar.gz -O - | sudo tar -xz -C /service/
+
 time=$(ls /service | grep arvan)
+
 echo "::set-output name=time::$time"
