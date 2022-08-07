4ca230ee7b8458eac61c6a4b8a1388645a0f0da0
Arash Hatami <hatamiarash7@gmail.com>
2022-08-07 12:32:21 +0430

    complete script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,22 @@ APP="$3"
 CONTAINER="$4"
 IMAGE="$5"
 
-echo "Hello"
+echo "*****************************************"
+echo "*                                       *"
+echo "*   Welcome to ArvanCloud PaaS Action   *"
+echo "*                                       *"
+echo "*****************************************"
 
 mkdir -p /service
 
+echo " -----> Download ArvanCloud CLI tool version: $CLI_VERSION"
+
 wget https://github.com/arvancloud/cli/releases/download/v${CLI_VERSION}/arvan_${CLI_VERSION}_linux_amd64.tar.gz -O - | tar -xz -C /service/
 
-time=$(ls /service | grep arvan)
+echo " -----> Login ..."
 
-echo "::set-output name=time::$time"
+/service/arvan login <<< """$AUTH"""
+
+echo " -----> Deploy ..."
+
+arvan paas set image deployment ${APP} ${CONTAINER}=${IMAGE}
